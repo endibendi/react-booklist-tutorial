269d7ad82268b2dda94a29c65c0c4fa2b97aebd1
Endre <endibend@gmail.com>
2021-06-26 17:39:41 +0200

    added props to book component

--- src/index.js
+++ src/index.js
@@ -3,27 +3,45 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+//setup variables
+const firstBook = {
+  imgUrl:
+    "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
+  title: "Atomic Habits: An Easy & Proven Way to…",
+  author: "James Clear",
+};
+const secoundBook = {
+  imgUrl:
+    "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg",
+  title: "The Very Hungry Caterpillar",
+  author: "Eric Carle",
+};
+
 const Booklist = () => {
   return (
     <>
       <section className="booklist">
-        <Book />
+        <Book
+          img={firstBook.imgUrl}
+          title={firstBook.title}
+          author={firstBook.author}
+        />
+        <Book
+          img={secoundBook.imgUrl}
+          title={secoundBook.title}
+          author={secoundBook.author}
+        />
       </section>
     </>
   );
 };
 
-const Book = () => {
-  const title = "Atomic Habits: An Easy & Proven Way to…";
-  const author = "James Clear";
-  const imgUrl =
-    "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg";
-
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={imgUrl} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
