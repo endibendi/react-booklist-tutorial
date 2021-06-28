a47fbf1ed056832076d36bbe834b2adad744da8d
Endre <endibend@gmail.com>
2021-06-28 15:08:09 +0200

    added basic events

--- src/index.js
+++ src/index.js
@@ -39,11 +39,33 @@ const Booklist = () => {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  // events in react
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}>
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
